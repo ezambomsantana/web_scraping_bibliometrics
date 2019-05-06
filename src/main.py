cb46fe69c0c35c18a5e18bd1e51986aac236338f
Eduardo Santana <ezambomsantana@gmail.com>
2019-05-06 18:50:51 -0300

    add graph

--- src/main.py
+++ src/main.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup
 import sys  
 import re
+import networkx as nx
 
 reload(sys)  
 sys.setdefaultencoding('utf8')
 
 url_ecc = "http://papers.cumincad.org/cgi-bin/works/Search?search=&paint=on&f%3A1=year&e%3A1=%3E%3D+x&v%3A1=1998&f%3A2=year&e%3A2=%3C%3D+x&v%3A2=2018&f%3A3=source&e%3A3=%3D~+m%2Fx%2Fi&v%3A3=eCAADe&f%3A4=&e%3A4=&v%3A4=&f%3A5=&e%3A5=&v%3A5=&grouping=and&days=&sort=DEFAULT&sort1=&sort2=&sort3=&max=3000&fields=id&fields=authors&fields=year&fields=title&fields=source&fields=summary&fields=WOS&fields=keywords&fields=series&fields=type&fields=email&fields=more&fields=content&fields=fullText&fields=references&fields=seeAlso&_form=AdvancedSearchForm&_formname=&format=LONG&frames=NONE"
-url_sigradi = "http://papers.cumincad.org/cgi-bin/works/Search?search=&paint=on&f%3A1=year&e%3A1=%3E%3D+x&v%3A1=1998&f%3A2=year&e%3A2=%3C%3D+x&v%3A2=2018&f%3A3=source&e%3A3=%3D~+m%2Fx%2Fi&v%3A3=sigradi&f%3A4=&e%3A4=&v%3A4=&f%3A5=&e%3A5=&v%3A5=&grouping=and&days=&sort=DEFAULT&sort1=&sort2=&sort3=&max=30000&fields=authors&fields=year&fields=title&fields=source&fields=keywords&fields=references&_form=AdvancedSearchForm&_formname=&format=LONG&frames=NONE"
+url_sigradi = "http://papers.cumincad.org/cgi-bin/works/Search?search=&paint=on&f%3A1=year&e%3A1=%3E%3D+x&v%3A1=1998&f%3A2=year&e%3A2=%3C%3D+x&v%3A2=2018&f%3A3=source&e%3A3=%3D~+m%2Fx%2Fi&v%3A3=sigradi&f%3A4=&e%3A4=&v%3A4=&f%3A5=&e%3A5=&v%3A5=&grouping=and&days=&sort=DEFAULT&sort1=&sort2=&sort3=&max=100&fields=authors&fields=year&fields=title&fields=source&fields=keywords&fields=references&_form=AdvancedSearchForm&_formname=&format=LONG&frames=NONE"
 page = requests.get(url_sigradi)
 
 soup = BeautifulSoup(page.text, 'html.parser')
@@ -17,6 +18,9 @@ soup = BeautifulSoup(page.text, 'html.parser')
 items = soup.find_all(class_='DATA')
 
 i = 0
+
+
+G = nx.Graph()
 
 reference_year = []
 author_year = []
@@ -26,6 +30,8 @@ keyword_year = []
 authors = []
 title = ''
 year = 0
+
+dict_keywords = {}
 
 i = 1
 for artist_name in items:
@@ -44,10 +50,25 @@ for artist_name in items:
         title = data.contents[0]
 
     if header.contents[0] == 'keywords':
-        kws = data.contents[0].replace(',',';').replace('/',';').split(';')  
+        kws = data.contents[0].replace(',',';').replace('/',';').split(';') 
         for k in kws:
             if k != '':
-                keyword_year.append([k.strip().lower(), year])             
+                G.add_node(k)
+                keyword_year.append([k.strip().lower(), year])  
+                achou = False
+                for k2 in kws:
+                    if (k == k2):
+                        achou = True
+                    if achou == True and k != k2:
+                        p1 = k.strip().lower()
+                        p2 = k2.strip().lower()
+                        if G.has_edge(p1, p2):
+                            G[p1][p2]['weight'] += 0.2
+                        elif G.has_edge(p2, p1):
+                            G[p2][p1]['weight'] += 0.2
+                        else:
+                            G.add_edge(p1, p2, weight=1)
+                 
         
     if header.contents[0] != 'references':
         print(data.contents[0])
@@ -70,7 +91,14 @@ for artist_name in items:
             except:
                 print("An exception occurred")
 
+pos = nx.circular_layout(G)
+weights = [G[u][v]['weight'] * 4 for u,v in G.edges()]
+nx.draw_networkx(G, pos=pos)
+nx.draw_networkx_edges(G,pos,width=weights, edge_color='g', arrows=False)
+plt.show()
 
+for u,v,a in G.edges(data=True):
+    print u, ';', v, ':',a['weight']
 
 ###### get number of papers by year
 
